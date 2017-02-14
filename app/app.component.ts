0a28153dc62866d4e931392a4cf5f71c6b8f5956
Courtney <courtney@epicodus.com>
2017-02-14 11:20:32 -0800

    Define currentFocus variable in root component class to contain current focus of To Do list; render variable in view with handlebar tags.

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,11 @@ import { Component } from '@angular/core';
   template: `
     <div class="container">
       <h1>To Do List</h1>
+      <h3>{{currentFocus}}</h3>
     </div>
   `
 })
 
 export class AppComponent {
-
+  currentFocus: string = 'Angular Homework';
 }
