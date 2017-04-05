d27feb12d5e023a81a402a2cac68fc54806e8e0a
Courtney Phillips <courtney@epicodus.com>
2017-04-05 15:20:37 -0700

    Add FormsModule to imports array in root module, to make available to rest of application.

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,8 @@ import { AppComponent }   from './app.component';
 import { FormsModule }  from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule,
+            FormsModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
