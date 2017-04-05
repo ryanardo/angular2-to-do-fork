6590bea2ea23019facc088f0a78eb1aa8c5c30f1
Courtney Phillips <courtney@epicodus.com>
2017-04-05 15:22:15 -0700

    Move line defining selectedTask to under tasks array.

--- app/app.component.ts
+++ app/app.component.ts
@@ -28,7 +28,6 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  selectedTask: Task = this.tasks[0];
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
@@ -38,6 +37,8 @@ export class AppComponent {
     new Task('Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)  ];
+  selectedTask: Task = this.tasks[0];
+
 
   editTask() {
     alert("You just requested to edit a Task!");
